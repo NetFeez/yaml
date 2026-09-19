1e2970027031297cf0b3112b89c0b8f825006a6a
NetFeez <netfeez.dev@gmail.com>
2026-09-19 01:06:37 +0000

    fix: added index module exports
    - Added index.html with module exports.
    - Bump version to 0.1.1

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './AST.js';
+export { Adapter } from './Adapter.js';
+export { Compiler, Compiler as default } from './Compiler.js';
+export { Parser } from './Parser.js';
+export { Serializer } from './Serializer.js';
+export { Synchronizer } from './Synchronizer.js';
+export { YamlError } from './YamlError.js';
